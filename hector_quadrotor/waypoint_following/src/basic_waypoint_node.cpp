07bbcd883dbddd973031b871f6e3efe71aaa8c97
alicsalman1 <alicsalman1@gmail.com>
2017-08-22 16:10:24 -0400

    update

--- hector_quadrotor/waypoint_following/src/basic_waypoint_node.cpp
+++ hector_quadrotor/waypoint_following/src/basic_waypoint_node.cpp
@@ -151,7 +151,7 @@ int main(int argc, char **argv) {
 	ros::NodeHandle nh,nh2;
 	ros::NodeHandle n;
 	// ros::Subscriber sub = n.subscribe("/robot_traj", 5, chatterCallback);
-	// // ros::Rate loop_rate( 10 );	
+	ros::Rate loop_rate( 10 );	
 	// while(flag<5)
 	// {
 		
@@ -235,7 +235,7 @@ int main(int argc, char **argv) {
 		
 		//Update subscribers and sleep
 		ros::spinOnce();
-		// loop_rate.sleep();
+		loop_rate.sleep();
 		
 		
 	}
